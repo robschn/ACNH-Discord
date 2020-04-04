54beaf9c18eca93711498d2b7fe8a0d97a19ceb0
Robert Schneider <robmschn@gmail.com>
2020-04-04 16:00:09 -0400

    changed vars

--- bot.py
+++ bot.py
@@ -9,10 +9,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='purchased_price', help='Logs purchase price of turnips')
-async def price(ctx, purchased_price: int):
-    await ctx.send(f'Your purchase price of {purchased_price} has been logged!')
-    return purchased_price
+@bot.command(name='purchase_price', help='Logs purchase price of turnips')
+async def price(ctx, purchase_price: int):
+    await ctx.send(f'Your purchase price of {purchase_price} has been logged!')
+    return purchase_price
 
 @bot.command(name='current_price', help='Logs current price of turnips')
 async def price(ctx, current_price: int):
@@ -21,11 +21,11 @@ async def price(ctx, current_price: int):
 
 @bot.command(name='stalk', help='Runs turnip price report')
 async def price(ctx):
-    purchase_price = purchase_price()
-    current_price = current_price()
+    return_purchase = purchase_price()
+    return_current = current_price()
     await ctx.send(
-    f'You purchased turnips at {purchase_price} bells',
-    f'Here is the current price: {current_price}'
+    f'You purchased turnips at {return_purchase} bells',
+    f'Here is the current price: {return_current}'
     )
 
 bot.run(TOKEN)
