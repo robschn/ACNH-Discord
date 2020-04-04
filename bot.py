dfd5255bf001e82f8a3b9f2ef558c8c3d45ed29a
Robert Schneider <robmschn@gmail.com>
2020-04-04 15:01:33 -0400

    testing message reply function

--- bot.py
+++ bot.py
@@ -13,4 +13,21 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
 client.run(TOKEN)
