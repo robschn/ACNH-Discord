fc991cd5d3543c76ff8538783ce2b9d3248857e4
Robert Schneider <robmschn@gmail.com>
2020-04-04 15:54:44 -0400

    adding report

--- bot.py
+++ bot.py
@@ -9,8 +9,23 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='turnip', help='Logs current price of turnips')
-async def price(ctx, current_price: int):
-    await ctx.send(f'Your current price of {current_price} has been logged!')
+@bot.command(name='purchased_price', help='Logs purchase price of turnips')
+async def price(ctx, turnip_price: int):
+    await ctx.send(f'Your purchase price of {turnip_price} has been logged!')
+    return turnip_price
+
+@bot.command(name='current_price', help='Logs current price of turnips')
+async def price(ctx, turnip_price: int):
+    await ctx.send(f'Your current price of {turnip_price} has been logged!')
+    return turnip_price
+
+@bot.command(name='stalk', help='Runs turnip price report')
+async def price(ctx):
+    purchase_price = purchase_price()
+    current_price = current_price()
+    await ctx.send(
+    f'You purchased turnips at {purchase_price} bells',
+    f'Here is the current price: {current_price}'
+    )
 
 bot.run(TOKEN)
