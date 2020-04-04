5aa622f068534de5ee1e8127f658cadc0faa0db9
Robert Schneider <robmschn@gmail.com>
2020-04-04 15:04:24 -0400

    added @client.event

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
