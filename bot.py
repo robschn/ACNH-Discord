f9e541217b061fe380db55d0b004b048293a1c7f
Robert Schneider <robmschn@gmail.com>
2020-04-04 15:18:28 -0400

    added back connect event

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
